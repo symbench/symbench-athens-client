aaac5efa1dc2804e0338be9bcc723a6c11a21a00
Timalsina <umesh.timalsina@vanderbilt.edu>
2021-08-03 19:36:35 -0500

    WIP- #3: Add models, job exection workflow

--- symbench_athens_client/athens_client.py
+++ symbench_athens_client/athens_client.py
@@ -45,9 +45,40 @@ class SymbenchAthensClient:
         return list(map(lambda x: x["fullname"], jobs)) if names_only else jobs
 
     def get_job_info(self, job_name):
+        """Get information about the job and its builds"""
         self.server.assert_job_exists(job_name)
         return self.server.get_job_info(job_name)
 
     def get_job_config(self, job_name):
+        """Get the jenkins configuration XML for the Job"""
         self.server.assert_job_exists(job_name)
         return self.server.get_job_config(job_name)
+
+    def can_execute(self):
+        """Return True if any worker nodes are connected"""
+        executor_nodes = list(
+            filter(lambda node: node["name"] != "master", self.server.get_nodes())
+        )
+        return not all(node["offline"] for node in executor_nodes)
+
+    def run_job_and_wait(self, job_name, params):
+        assert self.can_execute(), "None of the executor nodes are online/connected"
+        build_number = self.server.build_job(job_name, parameters=params)
+        return build_number
+
+    def get_job_status(self, build_number):
+        """Check the job status based on build number"""
+        print(self.server.get_build_info(build_number))
+
+
+if __name__ == "__main__":
+    client = SymbenchAthensClient(
+        jenkins_url="http://localhost:8080", username="symcps", password="symcps2021"
+    )
+
+    build_number = client.run_job_and_wait(
+        "cloneDesign",
+        {"FromDesignName": "QuadCopter", "ToDesignName": "QuadCopterCopy2"},
+    )
+
+    print(client.get_job_status(build_number))
