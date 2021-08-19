7226249783b1f8d2676a9bc0f6133b608562745e
Timalsina <umesh.timalsina@vanderbilt.edu>
2021-08-19 00:48:00 -0500

    WIP- Follow SWRi style for executing on a single design

--- symbench_athens_client/fd_executor.py
+++ symbench_athens_client/fd_executor.py
@@ -1,21 +1,31 @@
+import math
+import os
 import subprocess
-import tempfile
+from csv import DictWriter
 from pathlib import Path
+from shutil import move
+from uuid import uuid4
 
 from symbench_athens_client.exceptions import FDMFailedException
-from symbench_athens_client.utils import lhs_sampling
+from symbench_athens_client.models.designs import QuadCopter
+from symbench_athens_client.models.fd_metrics import (
+    FDMFlightMetric,
+    FDMFlightPathMetric,
+)
+from symbench_athens_client.utils import get_logger
 
 
 class FDMExecutor:
-    def __init__(self, fdm_path):
+    def __init__(self, fdm_path=None):
         """The executor for fdm process.
 
         Parameters
         ----------
-        fdm_path: str
-            The full path of the new_fdm.exe or new_fdm compiled on a linux system
+        fdm_path: str, default=None
+            The full path of the new_fdm.exe or new_fdm compiled on a linux system (can be none if its already in your path)
         """
-        self.fdm_path = fdm_path
+        self.fdm_path = fdm_path or "new_fdm"
+        self.logger = get_logger(self.__class__.__name__)
 
     def execute(self, input_file, output_file):
         """Execute the FDM process.
@@ -27,32 +37,119 @@ class FDMExecutor:
         output_file: str
             The output file path for the flight dynamics software
         """
-        with subprocess.Popen(
-            f"{self.fdm_path} < {input_file} > output_file", shell=True
-        ) as fdm_process:
+        fdm_cmd = f"{self.fdm_path} < {input_file} > {output_file}"
+        self.logger.info(f"Opening the FDM execution process as {fdm_cmd}")
+        with subprocess.Popen(fdm_cmd, shell=True) as fdm_process:
             try:
                 fdm_process.wait(300)
                 if fdm_process.returncode != 0:
                     raise FDMFailedException(
                         f"The FDM executable failed. The stderr is:\n{fdm_process.stderr}"
                     )
-                return self.parse_fdm_output(output_file)
+
+                return FDMFlightMetric.from_fd_metrics(
+                    "metrics.out"
+                ), FDMFlightPathMetric.from_fd_metrics("metrics.out")
+
             except subprocess.TimeoutExpired:
                 raise FDMFailedException("The FDM Process timed-out. Exiting.")
 
-    @staticmethod
-    def parse_fdm_output(output_file):
-        with open(output_file) as fdm_output:
-            metrics_start = False
-            metrics = []
-            for line in fdm_output:
-                if line.strip().startswith("#Metrics"):
-                    print("here")
-                    metrics_start = True
-                if line and metrics_start:
-                    metrics.append(line.strip())
-            return metrics
+
+def _update_total_score(metrics):
+    scores = [
+        metrics["Path_score_Path1"],
+        metrics["Path_score_Path3"],
+        metrics["Path_score_Path4"],
+        metrics["Path_score_Path5"],
+    ]
+    metrics["TotalPathScore"] = sum(scores) if not math.isclose(scores[2], 0.0) else 0.0
 
 
-class FDMSampler:
-    pass
+def _write_output_csv(output_dir, metrics):
+    output_csv = output_dir / "output.csv"
+    should_write_header = False
+    if not output_csv.exists():
+        should_write_header = True
+
+    with open(output_csv, "a") as csv_file:
+        csv_writer = DictWriter(csv_file, fieldnames=list(metrics.keys()))
+        if should_write_header:
+            csv_writer.writeheader()
+        csv_writer.writerow(metrics)
+
+
+def execute_fd_all_paths(
+    design,
+    tb_data_location,
+    requested_vertical_speed=10.0,
+    requested_lateral_speed=1,
+    propellers_data_location=f"../data/propellers",
+    fdm_path=None,
+    output_dir="results",
+):
+    """Execute flight dynamics on all paths for a design(only works for quadcopter).
+
+    Parameters
+    ----------
+    design: QuadCopter
+        The quadCopter seed design instance
+    tb_data_location: str, pathlib.Path
+        The testbench data location to compute mass properties for this design
+    requested_vertical_speed: float, default = 10.0
+        The requested vertical speed for this run
+    requested_lateral_speed: float, default = 1.0
+        The requested lateral speed for this run
+    propellers_data_location: str, pathlib.Path, default="../data/propellers"
+        The location for the propellers data
+    fdm_path: str, pathlib.Path, default=None
+        The fdm executable path (If None, it is assumed that fdm is in your path)
+    output_dir: str, patlib.Path, default="results"
+        Where to save the output to
+
+    Returns
+    -------
+    The contents of output.csv (Metrics) as a dictionary
+    """
+
+    assert isinstance(
+        design, QuadCopter
+    ), "The function only works for quadcopter design"
+
+    output_dir = Path(output_dir).resolve()
+    if not output_dir.exists():
+        os.makedirs(output_dir)
+    run_guid = str(uuid4())
+    fd_files_base_path = output_dir / run_guid
+    os.makedirs(fd_files_base_path, exist_ok=True)
+
+    executor = FDMExecutor(fdm_path=fdm_path)
+
+    metrics = {"GUID": run_guid}
+    for i in [1, 3, 4, 5]:
+        fd_input_path = f"FlightDynamicsPath{i}.inp"
+        fd_output_path = f"FlightDynamicsPath{i}.out"
+        design.to_fd_input(
+            test_bench_path=str(tb_data_location),
+            requested_vertical_speed=0 if i != 4 else requested_vertical_speed,
+            requested_lateral_speed=0 if i == 0 else int(requested_lateral_speed),
+            flight_path=i,
+            propellers_data_path=str(propellers_data_location),
+            filename=fd_input_path,
+        )
+
+        flight_metrics, path_metrics = executor.execute(
+            str(fd_input_path), str(fd_output_path)
+        )
+
+        metrics.update(flight_metrics.to_csv_dict())
+        metrics.update(path_metrics.to_csv_dict())
+
+        move(fd_input_path, fd_files_base_path)
+        move(fd_output_path, fd_files_base_path)
+
+    metrics.update(design.parameters())
+    _update_total_score(metrics)
+
+    _write_output_csv(output_dir=output_dir, metrics=metrics)
+
+    return metrics
--- symbench_athens_client/models/fixed_bemp_designs.py
+++ symbench_athens_client/models/fixed_bemp_designs.py
@@ -21,3 +21,23 @@ class TurnigyGraphene5000MAHQuadCopter(QuadCopter):
         self.propeller_1 = Propellers.apc_propellers_6x4E
         self.propeller_2 = Propellers.apc_propellers_6x4EP
         self.propeller_3 = Propellers.apc_propellers_6x4E
+
+
+class Batt60006S_Prop10x4_5MR_MotMN3508KV380QuadCopter(QuadCopter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Motors
+        self.motor_0 = Motors.t_motor_MN3508KV380
+        self.motor_1 = Motors.t_motor_MN3508KV380
+        self.motor_2 = Motors.t_motor_MN3508KV380
+        self.motor_3 = Motors.t_motor_MN3508KV380
+
+        # Propellers
+        self.propeller_0 = Propellers.apc_propellers_10x4_5MRP
+        self.propeller_1 = Propellers.apc_propellers_10x4_5MR
+        self.propeller_2 = Propellers.apc_propellers_10x4_5MR
+        self.propeller_3 = Propellers.apc_propellers_10x4_5MRP
+
+        # Battery
+        self.battery_0 = Batteries.TurnigyGraphene6000mAh6S75C
